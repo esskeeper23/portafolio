8b127a254ac64eacc451c8fe8669457c119938a6
Daniela Martinez <rene2.martinez@outlook.com>
2022-06-29 21:35:04 -0700

    open menu, body no-scroll

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
+const noScroll = document.querySelector(".scroll");
 
 function toggleMenu() {
     menu.classList.toggle("menu_opened");
+    noScroll.classList.toggle("no-scroll");
 }
 
 openMenuBtn.addEventListener("click", toggleMenu);
