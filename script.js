5e3aff06ac94f46696cdfd38f06deb366cf0ff21
Daniela Martinez <rene2.martinez@outlook.com>
2022-08-01 22:39:40 -0700

    loader gif, script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const noScroll = document.querySelector(".scroll");
+const loader = document.getElementById("preloader");
 
 function toggleMenu() {
     menu.classList.toggle("menu_opened");
@@ -10,3 +11,12 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+
+
+function vanish() {
+    loader.classList.add("disappear");
+    noScroll.classList.toggle("no-scroll");
+}
+
+setTimeout( vanish , 2000);
